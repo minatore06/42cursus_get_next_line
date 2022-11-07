38f92b6283f67428fdfa786b9b6d7423a0e4436e
Stefano Caiazzo <scaiazzo@c2r2p9.42firenze.it>
2022-11-07 18:28:37 +0100

    better future

--- get_next_line.c
+++ get_next_line.c
@@ -15,7 +15,7 @@
 
 #include <stdio.h>
 
-static void	resetbfr(char **buffer, unsigned int size)
+void	resetbfr(char **buffer, unsigned int size)
 {
 	while(size-- > 0)
 		(*buffer)[size] = 0;
@@ -37,41 +37,10 @@ int	ft_strchr(const char *s, int c)
 	return (-1);
 }
 
-static char	*past_check(char **safe)
+int	get_line_save_rest(char **safe, char **line, char **buffer)
 {
 	int	i;
-	char	*line;
 
-	i = 0;
-	if (*safe)
-	{
-		if (**safe)
-		{
-			line = ft_strdup(*safe);
-			i = ft_strchr(line, '\n');
-			free(*safe);
-			safe = 0;
-			if (i != -1)
-			{
-				*safe = ft_strdup(&line[i + 1]);
-				line[i + 1] = 0;
-				return (line);
-			}
-		}
-	}
-	return (NULL);
-}
-
-int	get_line_save_rest(int i, int x, char **buffer, char **safe, char **line)
-{
-	if (x <= 0)
-	{
-		free(*buffer);
-		if (!*line)
-			*line = NULL;
-		return (-1);
-	}
-	*buffer[BUFFER_SIZE] = 0;
 	i = ft_strchr(*buffer, '\n');
 	if (i == -1)
 		i = ft_strchr(*buffer, -1);
@@ -87,25 +56,18 @@ int	get_line_save_rest(int i, int x, char **buffer, char **safe, char **line)
 		*line = ft_strjoin(*line, *buffer);
 		return (1);
 	}
+	*line = ft_strjoin(*line, *buffer);
 	return (0);
 }
 
-char	*get_next_line(int fd)
+char	*reading(int i, int fd, char **safe, char *line)
 {
-	int		i;
-	int		x;
 	char	*buffer;
-	char	*line;
-	static char	*safe;
+	int	x;
 
-	line = past_check(&safe);
-	if (line)
-		return (line);
 	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buffer)       
 	{
-		if (line)
-			free(line);
 		free(buffer);
 		return (NULL);
 	}
@@ -114,14 +76,45 @@ char	*get_next_line(int fd)
 	{
 		resetbfr(&buffer, BUFFER_SIZE + 1);
 		x = read(fd, buffer, BUFFER_SIZE);
-		i = get_line_save_rest(0, x, &buffer, &safe, &line);
-		if (i == 1)
-			break;
-		if (i == -1)
+		if (x <= 0)
+		{
+			free(buffer);
+			if (!line)
+				line = NULL;
 			return (line);
-		line = ft_strjoin(line, buffer);
+		}
+		buffer[BUFFER_SIZE] = 0;
+		i = get_line_save_rest(safe, &line, &buffer);
+		if (i)
+			break;
 	}
 	free(buffer);
 	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	int		i;
+	char	*line;
+	static char	*safe;
+
+	line = 0;
+	if (safe)
+	{
+		if (*safe)
+		{
+			line = ft_strdup(safe);
+			i = ft_strchr(line, '\n');
+			free(safe);
+			safe = 0;
+			if (i != -1)
+			{
+				safe = ft_strdup(&line[i + 1]);
+				line[i + 1] = 0;
+				return (line);
+			}
+		}
+	}
+	return (reading(0, fd, &safe, line));
 }
 
